43d061291c02a3d12f11576c3b7d7312b5be212c
Scott Brady <scott@prolio.com>
2016-08-29 17:29:38 -0400

    Added Hello World

---
+++ public/examples/HelloWorld.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Program Name: Hello World
+   Programmer:   Scott Brady
+   Instructor:   Mr. Brady for Introduction to Programming
+   Date Due:     8/26/2016
+ PLEDGE STATEMENT
+ ----------------
+   I pledge that all of the code in this program is my own _original_ work for 
+   the 2016-2017 school year. None of the code in this program has been copied 
+   from anyone or anyplace unless I was specifically authorized to do so by my
+   teacher. This program has been created using properly licensed software.
+                                     Signed: ________________________________
+ DOCUMENTATION
+ -------------
+   This program gives a greeting to the whole world.
+ ******************************************************************************/
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
                 <div class="panel-body">
                     <h3>Welcome!</h3>
                     <p>Check back here later to see announcements</p>
+                    <p><a href="examples/HelloWorld.java" target="_blank">Hello World program</a></p>
                     <hr />
                     <p>If you already lost your Course Outline, download it <a href="https://drive.google.com/open?id=1jqyVPgCTojW3CaMqMmcEijIUSeR97JmUDz3In3rEJ0k" target="_blank">here</a></p>
                     <p>Download the Java Development Kit <a href="http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html" target="_blank">here</a></p>
