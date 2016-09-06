c38ff8f6d3b408775d6f6f73c0c9a1ddbb5f093c
Scott Brady <scott@prolio.com>
2016-09-06 10:46:59 -0400

    Updating home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,12 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <h3>Welcome!</h3>
-                    <p>Check back here later to see announcements</p>
-                    <p><a href="examples/HelloWorld.java" target="_blank">Hello World program</a></p>
+                    <ul>
+                        <li><p>If you stil need to download the JDK, go to the reading page, I put up an article on how to install it.</p></li>
+                        <li><p>Download the Hello World program <a href="examples/HelloWorld.java" target="_blank">here</a></p></li>
+                    </ul>
                     <hr />
                     <p>If you already lost your Course Outline, download it <a href="https://drive.google.com/open?id=1jqyVPgCTojW3CaMqMmcEijIUSeR97JmUDz3In3rEJ0k" target="_blank">here</a></p>
-                    <p>Download the Java Development Kit <a href="http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html" target="_blank">here</a></p>
                 </div>
             </div>
         </div>
