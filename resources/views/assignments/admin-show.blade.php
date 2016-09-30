90c028a3ea2b10bb4dc569a2e4b673a3b8528c50
Scott Brady <scott@prolio.com>
2016-09-30 17:21:14 -0400

    Same thing with admin panel

--- resources/views/assignments/admin-show.blade.php
+++ resources/views/assignments/admin-show.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <h1 class="page-header">{{$assignment->name}}</h1>
 
-            <h3>{{$assignment->description}}</h3>
+            <h3>{!!$assignment->description!!}</h3>
             <h5>Due: {{$assignment->due->format('M. jS')}}</h5>
             
             <div class="panel panel-default">
