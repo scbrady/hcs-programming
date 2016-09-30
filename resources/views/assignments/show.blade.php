6bd9ff225d00a0ef196300257de961339818c185
Scott Brady <scott@prolio.com>
2016-09-30 17:13:58 -0400

    Allow html formatting in description

--- resources/views/assignments/show.blade.php
+++ resources/views/assignments/show.blade.php
@@ -126,7 +126,7 @@
             </div>
 
             <h1 class="page-header">{{$assignment->name}}</h1>
-            <h3>{{$assignment->description}}</h3>
+            <h3>{!!$assignment->description!!}</h3>
             <h5>Due: {{$assignment->due->format('M. jS')}}</h5>
             
             <div class="panel panel-default">
